07deb0da0c031529f99b101941d1960e699ca190
Sergei <loft998@yandex.ru>
2022-12-16 00:08:31 +0300

    Zadach 4_2 Ready

--- Home_work/Zadacha_4/Zadacha_4.cs
+++ Home_work/Zadacha_4/Zadacha_4.cs
@@ -4,11 +4,26 @@
 // Обявляем массив
 int[] chisla = new int[3];
 int i = 0;
-/*
+
 // Заполнение массива
 Console.WriteLine("Введите числа");
 while(i < 3)
 {
-    chislo[i] = Convert.ToInt32(Console.ReadLine());
+    chisla[i] = Convert.ToInt32(Console.ReadLine());
     i++;
-}*/
+}
+
+// Поиск максимального
+int max = chisla[0];
+i = 1;
+while(i < chisla.Length)
+{
+    if(chisla[i] > max)
+    {
+        max = chisla[i];
+    }
+    i++;
+}
+
+//Вывод
+Console.WriteLine($"Максимальное число: {max}");
