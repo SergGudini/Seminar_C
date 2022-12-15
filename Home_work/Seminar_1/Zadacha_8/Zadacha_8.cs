78989a60d21caa2e800f3455bd38497457db15a3
Sergei <loft998@yandex.ru>
2022-12-16 00:37:45 +0300

    Zadacha_8 Ready

--- Home_work/Seminar_1/Zadacha_8/Zadacha_8.cs
+++ Home_work/Seminar_1/Zadacha_8/Zadacha_8.cs
@@ -1,3 +1,17 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
-Console.WriteLine("Hello, World!");
+
+// Ввод данных 
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+// Поиск четных чисел от 1 до chislo
+while(i <= chislo)
+{
+    if ((i % 2) == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
