ea662a2e6479cf0e40366ca6f5c3f143a182f945
Sergei <loft998@yandex.ru>
2022-12-15 23:46:21 +0300

    Zadach 4_2

--- Home_work/Zadacha_4/Zadacha_4.cs
+++ Home_work/Zadacha_4/Zadacha_4.cs
@@ -1,14 +1,20 @@
 // Задача 4: Напишите программу, которая принимает на вход три числа 
 //и выдаёт максимальное из этих чисел. 
 
-// Обявляем длину массива
-int[] qwe = { 1, 2, 3 }
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Обявляем длину массива
+        int[] qwe = { 1, 2, 3 };
 int i = 0;
+    }
+}
 /*
 // Заполнение массива
 Console.WriteLine("Введите числа");
 while(i < 3)
 {
-    chislo[i] = Convert.ToInt32(Console.ReadLine());
-    i++;
+   chislo[i] = Convert.ToInt32(Console.ReadLine());
+   i++;
 }*/
