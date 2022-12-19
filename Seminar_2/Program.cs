52c864fbf984685007ccf285da322de00388538f
Sergei <loft998@yandex.ru>
2022-12-19 20:46:40 +0300

    Рандом

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int number = new Random().Next(10,100); //рандом от 10 до 99
+int result = number / 10;
+Console.WriteLine(result);
+Console.WriteLine(number);
