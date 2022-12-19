6c309b254ceb13eaacb73e173c8f912c8c3d8fc2
Sergei <loft998@yandex.ru>
2022-12-19 23:54:06 +0300

    Zadacha 13

--- Home_work/Seminar_2/Zadacha_13/Program.cs
+++ Home_work/Seminar_2/Zadacha_13/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int size = 0;
+int result = number;
+int i = 0;
+
+while (number != 0) //определяем размер введенного числа
+{
+    number = number / 10;
+    size ++; 
+}
+
+if (size >= 3) // проверяем количество введенных цифр, если 3 и больше выводим 3-ю цифру
+{
+    while (i < (size - 3)) // делим столько раз пока не останется 3 цифры
+    {
+        result = result / 10;
+        i++;
+    }
+    
+    Console.Write($"Третья цифра: {Math.Abs(result % 10)}"); // выводи 3-ю цифру
+}
+else Console.Write("Третьей цифры нет"); // иначе 3-ей цифры нет
