db6d0ed71e082f060c7852a6307089ac5b2c1c9d
Sergei <loft998@yandex.ru>
2022-12-22 20:59:14 +0300

    Zadacha 3 seminar

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 18. Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+/*Console.WriteLine("Введите номер четверти от 1 до 4");
+int part = Convert.ToInt32(Console.ReadLine());
+
+if (part >= 1 && part <= 4)
+{
+    if (part == 1)
+    {
+        Console.WriteLine("x > 0 y > 0");
+    }
+    else if (part == 2)
+    {
+        Console.WriteLine("x < 0 y > 0");
+    }
+    else if (part == 3)
+    {
+        Console.WriteLine("x < 0 y < 0");
+    }
+    else if (part == 4)
+    {
+        Console.WriteLine("x > 0 y < 0");
+    }
+}
+else Console.WriteLine("Номер четверти не подходит");*/
+
+// Задача 21. Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+Console.Write("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+Console.Write($"Расстояние: {result}");
