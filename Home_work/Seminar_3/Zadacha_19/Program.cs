ba2a5cef509a05129f1f1619e43017e0c33ae47f
Sergei <loft998@yandex.ru>
2022-12-22 21:46:40 +0300

    Цикл

--- Home_work/Seminar_3/Zadacha_19/Program.cs
+++ Home_work/Seminar_3/Zadacha_19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19: Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+int number = 0;
+void start(int x)
+{
+    Console.Write("Введите пятизначное число: "); // ввод числа
+    number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int size = 0;
+while (number != 0) //определяем размер введенного числа
+{
+    number = number / 10;
+    size ++; 
+}
+
+Console.Write(size);
+return start;
