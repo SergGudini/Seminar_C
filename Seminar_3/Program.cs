7c64e401d2f5250787f81b98850f1c5af41c2708
Sergei <loft998@yandex.ru>
2022-12-22 21:46:03 +0300

    Циклы

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -25,10 +25,32 @@ if (part >= 1 && part <= 4)
 }
 else Console.WriteLine("Номер четверти не подходит");*/
 
+/*Console.Write("Введите координаты: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+        Console.WriteLine("x > 0 y > 0");
+        break;
+    case 2:
+        Console.WriteLine("x < 0 y > 0");
+        break;
+    case 3:
+        Console.WriteLine("x < 0 y < 0");
+        break;
+    case 4:
+        Console.WriteLine("x > 0 y < 0");
+        break;
+   default:
+        Console.WriteLine("Номер четверти не подходит");
+        break;
+}*/
+
 // Задача 21. Напишите программу, которая принимает на вход координаты 
 // двух точек и находит расстояние между ними в 2D пространстве.
 
-Console.Write("Введите x1: ");
+/*Console.Write("Введите x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите y1: ");
@@ -41,4 +63,15 @@ Console.Write("Введите y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
-Console.Write($"Расстояние: {result}");
+Console.Write($"Расстояние: {Math.Round(result,3)}");*/
+
+// ЦИКЛЫ for и while
+
+// while (условие) {...}
+int i = 1;
+while(i <= 10)
+{
+    Console.WriteLine("Hello");
+    i++;
+}
+// for ()
