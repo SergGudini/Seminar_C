c7de3af1c5533c7f0c331054ec4b2f75594f8b6d
Sergei <loft998@yandex.ru>
2022-12-22 22:42:51 +0300

    Zadacha 19_3

--- Home_work/Seminar_3/Zadacha_19/Program.cs
+++ Home_work/Seminar_3/Zadacha_19/Program.cs
@@ -1,18 +1,26 @@
 ﻿// Задача 19: Напишите программу, которая принимает на вход 
 // пятизначное число и проверяет, является ли оно палиндромом.
-int number = 0;
-void start(int x)
+
+Console.Write("Введите пятизначное число: "); // ввод числа
+int[] number = new int[5];
+int j = 4;
+int check = 0;
+
+for (int i = 0; i < 5; i++) {number[i] = Convert.ToInt32(Console.ReadLine());} // заполнение массива
+
+for (int i = 0; i < 4; i++) // сравнение первой цифры (i) и последней (j) и так до середины
 {
-    Console.Write("Введите пятизначное число: "); // ввод числа
-    number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
-int size = 0;
-while (number != 0) //определяем размер введенного числа
-{
-    number = number / 10;
-    size ++; 
+    if (number[i] == number [j]) 
+    {
+        check = 1; 
+        j--;
+    }
+    else 
+    {
+        check = 0;
+        break;   
+    }
 }
 
-Console.Write(size);
-return start;
+if (check == 1) Console.WriteLine("\r\nПолиндром"); // вывод
+else Console.WriteLine("\r\nНе полиндром");
