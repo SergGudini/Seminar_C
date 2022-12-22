2149090621b1933d56addf90699dd7a54b4ce1da
Sergei <loft998@yandex.ru>
2022-12-22 23:13:57 +0300

    Zadacha 23_3

--- Home_work/Seminar_3/Zadacha_23/Program.cs
+++ Home_work/Seminar_3/Zadacha_23/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23: Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) // если число положительное
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"{Math.Pow(i,3)} "); //возведение в степень 3
+    }
+}
+else if (number < 0) // если число отрицательное
+{
+    for (int i = number; i <= 1; i++)
+    {
+        Console.Write($"{Math.Pow(i,3)} ");
+    }
+}
+else Console.Write("Вы ввели некорректное число");
+
