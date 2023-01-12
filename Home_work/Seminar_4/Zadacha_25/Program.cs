4ef6122699c304d8176dff6be90f574ed5fc3bbe
Sergei <loft998@yandex.ru>
2023-01-12 22:42:08 +0300

    Задача 25 семинара 4 готово

--- Home_work/Seminar_4/Zadacha_25/Program.cs
+++ Home_work/Seminar_4/Zadacha_25/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+int Stepen (int number1, int number2)
+{
+    int i = 1;
+    int result = 1;
+    while (i <= number2)
+    {
+        result = result * number1;
+        i++;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int chislo_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int chislo_2 = Convert.ToInt32(Console.ReadLine());
+
+if(chislo_2 >= 0)
+{
+   Console.Write(Stepen(chislo_1,chislo_2));
+}
+else Console.Write("Вы ввели отрицательную степень. Поэтому конец."); 
+
+
