165bbb3287ff49f72d81bbf6bb052adc049edee6
Sergei <loft998@yandex.ru>
2023-01-12 23:26:15 +0300

    Задача 29 семинара 4 готово

--- Home_work/Seminar_4/Zadacha_29/Program.cs
+++ Home_work/Seminar_4/Zadacha_29/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+int[] EnterArray(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());;
+    }
+
+    return arr;
+}
+
+void ExitArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = EnterArray(size);
+ExitArray(array);
