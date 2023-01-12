e127c45d14680846f6faac25706809475238a9e0
Sergei <loft998@yandex.ru>
2023-01-12 23:15:09 +0300

    Задача 27 семинара 4 готово

--- Home_work/Seminar_4/Zadacha_27/Program.cs
+++ Home_work/Seminar_4/Zadacha_27/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+int SizeOfNumber (int number)
+{
+     int size = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        size ++; 
+    }
+    return size;
+}
+
+int SummNumber (int size, int number)
+{
+    int sum = 0;
+    while (size != 0)
+    {
+        sum = sum + (number % 10);
+        number = number / 10;
+        size--; 
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+int size = SizeOfNumber(Math.Abs(chislo));
+
+Console.Write($"Сумма чисел: {SummNumber(size,Math.Abs(chislo))}");
