b1c26b373ef21b154543039dc35b54af204e772f
Sergei <loft998@yandex.ru>
2023-01-22 14:23:54 +0300

    Домашняя задача семинар 6_41

--- Home_work/Seminar_6/Zadacha_41/Program.cs
+++ Home_work/Seminar_6/Zadacha_41/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop" и производится при помощи нажатия Enter
+
+string StopSlovo = "stop";
+string number = "";
+int count = 0;
+Console.WriteLine("Введите числа:");
+
+while (true)
+{
+    number = Console.ReadLine()!;
+
+    if(StopSlovo == number) break;
+    
+    if(Convert.ToInt32(number) > 0) count++; // проверка больше нуля
+}
+
+Console.WriteLine($"{count} число (числа, чисел) больше 0 ввёл пользователь");
