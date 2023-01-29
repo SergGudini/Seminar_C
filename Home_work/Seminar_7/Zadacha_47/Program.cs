a2b616ea32d39ff451f921b5c98977470eeb5a41
Sergei <loft998@yandex.ru>
2023-01-29 19:17:32 +0300

    Домашняя задача семинар 7_47

---
+++ Home_work/Seminar_7/Zadacha_47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] GetRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    double[,] array = new double[rows,columns];
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = leftRange + (new  Random().NextDouble() * (rightRange - leftRange)); // ранодмные вещественные числа с диапазоном от -9 до 9
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i,j], 2) + "  "); // округление до двух чисел после запятой
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 5;
+const int LEFTRANGE = -9;
+const int RIGHTRANGE = 9;
+
+double[,] array = GetRandomArray(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintArray(array);
---
+++ Seminar_8/Program.cs
@@ -0,0 +1,167 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива. 
+
+/*int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows,columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Change(int[,] matrix)
+{
+    int temp = 0;
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = temp;
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 5;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 9;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintMatrix(array);
+Console.WriteLine();
+Change(array);
+PrintMatrix(array);*/
+
+// Задача 55: Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы.
+
+/*int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows,columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] TMatrix(int[,] matrix)
+{
+    int[,] matrix2 = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+    for(int i = 0; i < matrix2.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            matrix2[i, j] = matrix[j, i];
+        }
+        Console.WriteLine();
+    }
+    return matrix2;
+}
+
+const int ROWS = 3;
+const int COLUMNS = 5;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 9;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintMatrix(array);
+Console.WriteLine();
+int[,] matrix2 = TMatrix(array);
+PrintMatrix(matrix2);*/
+
+// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается 
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows,columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Count(int[,] matrix)
+{
+    for(int temp = 0; temp <= 9; temp++)
+    {
+        int count = 0;
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                {
+                    if(matrix[i, j] == temp)
+                    {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        Console.WriteLine($"{temp} встречается {count} раз.");
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 5;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 9;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintMatrix(array);
+Console.WriteLine();
+Count(array);
+
