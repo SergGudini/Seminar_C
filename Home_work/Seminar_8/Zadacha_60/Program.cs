47f1e1f2fbab1b529b9c83fea4184503e4b2aff2
Sergei <loft998@yandex.ru>
2023-01-30 21:41:59 +0300

    Seminar 9

---
+++ Home_work/Seminar_8/Zadacha_60/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int[,,] GetRandom3Array(int rows, int columns, int z, int leftRange, int rightRange)
+{
+    int[,,] array = new int[rows,columns,z];
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i,j,k] = new Random().Next(leftRange, rightRange + 1);
+            }
+            
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i,j,k]} ({i},{j},{k}) ");
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 2;
+const int COLUMNS = 2;
+const int Z = 2;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 9;
+
+int[,,] array1 = GetRandom3Array(ROWS, COLUMNS, Z, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine("Первая матрица");
+PrintArray(array1);
---
+++ Seminar_9/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+/*void NaturalNumber(int N)
+{
+    if (N == 0) return;
+    NaturalNumber(N-1);
+    Console.Write(N + " ");
+}
+
+int number = 5;
+NaturalNumber(number);*/
+
+// Задача 65: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+/*void NaturalNumber(int N, int M)
+{
+    if (M == N + 1) return;
+
+    else Console.Write(M + " "); NaturalNumber(N, M + 1);
+    
+}
+
+int number1 = 4;
+int number2 = 8;
+NaturalNumber(number2, number1);*/
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SummaNumber(int number)
+{
+    int sum = 0;
+    if (number == 10) return 0;
+    else
+    {
+        sum = number % 10 + SummaNumber(number / 10);
+    }
+    return sum;
+}
+
+int N = 453;
+SummaNumber(N);
