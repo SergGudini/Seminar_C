b148b5b4b55e13c7651357d389b59d996fb63647
Sergei <loft998@yandex.ru>
2023-02-06 14:05:46 +0300

    Домашняя задача семинар 9_66

--- Home_work/Seminar_9/Zadacha_66/Program.cs
+++ Home_work/Seminar_9/Zadacha_66/Program.cs
@@ -4,11 +4,11 @@
 int NaturalNumber(int M, int N)
 {
     int sum = 0;
-    if (N == 0) return 0;
+    if (N == M-1) return 0;
     sum = N + NaturalNumber(M, N - 1);
     return sum;
 }
 
 int M = 1;
-int N = 5;
+int N = 15;
 Console.Write(NaturalNumber(M, N));
