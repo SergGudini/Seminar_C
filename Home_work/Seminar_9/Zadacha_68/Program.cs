14c0533f1314cc50bcbaea6d86b73c4a6a9431e0
Sergei <loft998@yandex.ru>
2023-02-06 14:32:26 +0300

    Домашняя задача семинар 9_68

--- Home_work/Seminar_9/Zadacha_68/Program.cs
+++ Home_work/Seminar_9/Zadacha_68/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int AkermanFunction(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (N == 0 && M > 0) return AkermanFunction(M - 1, 1);
+    else if (M > 0 && N > 0) return AkermanFunction(M - 1, AkermanFunction(M, N - 1));
+    else return 0;
+}
+
+int M = 2;
+int N = 3;
+Console.Write(AkermanFunction(M, N));
